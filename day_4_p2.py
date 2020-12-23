142ead82961d714f19d5b0fd9f3164bac3f4862c
Mathias <mathenderson@protonmail.com>
2020-12-23 13:50:39 -0300

    day4 - 168 too high

--- day_4_p2.py
+++ day_4_p2.py
@@ -46,64 +46,81 @@ with open('day_4_inputs.txt', 'r') as f:
             # identify line of data entry:
             elif char == '\n' and prev_char != '\n' and prev_char != ' ':
                 # take the entire line
-                temp.append(line[:-2]) #[:-2] avoids taking \n with it
+                temp.append(line[:-1]) #[:-2] avoids taking \n with it
             prev_char = char
         counter += 1
     
 
 
-    for elem in data[:3]:
+    for elem in data:
         # turn string into list of kv-pairs
         kv_pairs = elem.split(" ")
-        print(kv_pairs)
 
         # //// Part 2 //// 
         # Creating a dictionary
         keys = [segment.split(":")[0] for segment in kv_pairs]
         values = [segment.split(":")[1] for segment in kv_pairs]
         td = {}
-        print(keys)
-        print(values)
+        
+        # /// for Debugging:
+        print("\n", kv_pairs)
+        # print(keys)
+        # print(values)
 
         for i in range(len(kv_pairs)):
-            td[keys[i]] = [values[i]]   
+            td[keys[i]] = values[i]
         
-        # print(td)
+        print(td)
 
         flag = True
         while flag == True:
             # contains all required fields? (PART 1)
             if all([True if field in keys else False for field in req_fields]):
-                accepted_list.append(kv_pairs)
 
                 # required fields in correct format? (PART 2)
-                if len(td['byr']) != 4 or td['byr'] < 1920 or td['byr'] > 2002:    
+                if len(td['byr']) != 4 or int(td['byr']) < 1920 or int(td['byr']) > 2002:    
+                    print("Failed byr test")
                     flag = False
-                if len(td['iyr']) != 4 or td['iyr'] < 2010 or td['byr'] > 2020:    
+                if len(td['iyr']) != 4 or int(td['iyr']) < 2010 or int(td['iyr']) > 2020:    
+                    print("Failed iyr test")
                     flag = False
-                if len(td['eyr']) != 4 or td['iyr'] < 2020 or td['byr'] > 2030:    
+                if len(td['eyr']) != 4 or int(td['eyr']) < 2020 or int(td['eyr']) > 2030:    
+                    print("Failed eyr test")
                     flag = False
                 if td['hgt'][-2:] == 'cm':
-                    if td['hgt'][:-2] < 150 or td['hgt'][:-2] > 193:
+                    if int(td['hgt'][:-2]) < 150 or int(td['hgt'][:-2]) > 193:
+                        print("Failed hgt test - cm")
                         flag = False
                 if td['hgt'][-2:] == 'in':
-                    if td['hgt'][:-2] < 59 or td['hgt'][:-2] > 76:
+                    if int(td['hgt'][:-2]) < 59 or int(td['hgt'][:-2]) > 76:
+                        print("Failed hgt test - in")
                         flag = False
                 # the case that it's neither 'in' or 'cm'
                 if td['hgt'][-2:] != 'in' and td['hgt'][-2:] != 'cm':
+                    print("Failed hgt test - unit type")
                     flag = False
                 # Hex color pattern
                 pattern = '^#[a-f0-9]{6}'
                 if not re.match(pattern, str(td['hcl'])):
+                    print("Failed hcl test")
                     flag = False
                 if td['ecl'] not in eyc_list:
+                    print("Failed ecl test")
                     flag = False
                 # pid pattern 9 digits including 0's
                 pattern = '[0-9]{9}'
                 if not re.match(pattern, str(td['pid'])):
+                    print("Failed pid test")
                     flag = False
                 # ...
-                accepted_list.append(kv_pairs)
-            flag = False
-    # print(len(accepted_list))
-    print(accepted_list)
+                if flag == True:
+                    accepted_list.append(kv_pairs)
+                flag = False
+            else:
+                flag = False
+    
+    print(len(accepted_list))
+    
+    # # Debug:
+    # print("\n////////// ITEMS ACCEPTED ////////")
+    # print(accepted_list)
