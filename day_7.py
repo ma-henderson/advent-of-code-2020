4d6cddf0e5462186b6aa4fb12be19318719293ed
mathias <mathenderson@protonmail.com>
2020-12-30 15:35:22 +0400

    day_7 tests

---
+++ day_7.py
@@ -0,0 +1,12 @@
+with open("day_7_inputs", 'r') as f:
+  nodes = []
+  for line in f:
+    # Find all the nodes in which a shiny gold bag can be carried in
+
+    # Sentence structures include:
+    # Direct (without commas)
+    if line.count(",") == 0:
+      # Identify the in
+      
+
+    # Connection (with commas)
--- test.py
+++ test.py
@@ -10,5 +10,11 @@
 # print(int(n/2))
 
 
-day_6 = 'accddeeffhhijjkkllmmnnoopprrttvvxxyyzz'
-print(day_6.count('J'))
+# day_6 = 'accddeeffhhijjkkllmmnnoopprrttvvxxyyzz'
+# print(day_6.count('J'))
+
+day_7 = 'faded bronze bags contain 2 light salmon bags.'
+in_index = day_7.find('contain')
+in_count = in_index + 8
+in_color = day_7[in_count + 2,]
+print(in_index, day_7[in_index + 8])
