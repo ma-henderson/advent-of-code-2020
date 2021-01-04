d0ab9d0429bf2886ea34a1d361b9ba260c90c524
mathias <mathenderson@protonmail.com>
2021-01-04 19:39:02 +0400

    day8 p2 complete

--- day_8.py
+++ day_8.py
@@ -1,11 +1,14 @@
-with open('day_8_input.txt', 'r') as f:
-  # Set up base dictionary
-  lines = f.readlines()
-  data = {}
-  for i, n in enumerate(lines):
-    data[i] = n[:-1]
+def day8_data():
+  with open('day_8_input.txt', 'r') as f:
+    # Set up base dictionary
+    lines = f.readlines()
+    data = {}
+    for i, n in enumerate(lines):
+      data[i] = n[:-1]
 
-  print(data)
+    return data
+
+def day8_p1(data):  
   # Set up result variables
   acc = 0
   line_log = []
@@ -14,8 +17,16 @@ with open('day_8_input.txt', 'r') as f:
   # Start
   adr = 0
   while flag == True:
+    # for Part 2:
+    if adr >= len(data.keys()):
+      print("Booted succesfully by changing address", adr)
+      # append to log for checking
+      line_log.append(adr)
+      return acc, line_log
+
     if adr in line_log:
-      flag = False    
+      flag = False
+    
 
     # After checking if repeated, add adr to log
     line_log.append(adr)
@@ -43,5 +54,34 @@ with open('day_8_input.txt', 'r') as f:
     # Testing
     else:
       print('fail in reading c:', c)
+
+  return acc, line_log
+
+data_dict = day8_data()
+acc, line_log = day8_p1(data_dict)
+
+# Part 2:
+for adr in line_log:
+  # set up line's values
+  c = data_dict[adr].split()[0]
+  n = data_dict[adr].split()[1]
+  # keep a copy in case it doesn't fix the issue
+  temp_v = data_dict[adr]
+
+  # print(adr)
+
+  if c == 'jmp':
+    # change jmp to nop command
+    data_dict[adr] = ' '.join(['nop', n])
+  elif c == 'nop':
+    # change nop to jmp
+    data_dict[adr] = ' '.join(['jmp', n])
   
-  print(acc, line_log)
+  # run the program and see if it ends succesfully
+  acc, line_log_t = day8_p1(data_dict)
+  # last number > len means it terminated succ.
+  if line_log_t[-1] > list(data_dict.keys())[-1]:
+    print("\n", acc)
+  else:
+    data_dict[adr] = temp_v
+  
--- test_8.py
+++ test_8.py
@@ -1,12 +1,22 @@
-with open('day_8_input.txt', 'r') as f:
-  print(type(f))
-  lines = f.readlines()
-  print(lines[:10])
+# with open('day_8_input.txt', 'r') as f:
+#   print(type(f))
+#   lines = f.readlines()
+#   print(lines[:10])
   
-  data = {}
-  for i, n_lines in enumerate(lines[:10]):
-    print(i, n_lines[:-2])
-    data[i] = n_lines[:-2]
-  print(data)
+#   data = {}
+#   for i, n_lines in enumerate(lines[:10]):
+#     print(i, n_lines[:-2])
+#     data[i] = n_lines[:-2]
+#   print(data)
 
   
+def some_fn():
+  a = 'hello'
+  b = 'world'
+  return a,b
+
+s1, s2 = some_fn()
+print(s1, s2)
+
+s = some_fn()
+print(type(s))
